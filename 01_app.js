4fff7bd23115c0720680e76edf9bde76db09e699
James Delsoin <jamesdelson@hotmail.com>
2018-03-23 16:18:55 -0400

    Écouter le répertoire sur local:8081

--- 01_app.js
+++ 01_app.js
@@ -36,6 +36,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
 console.log('connexion à la BD')
 // lancement du serveur Express sur le port 8081
  app.listen(8081, (err) => {
+ server.listen(8081, (err) => {
  	if (err) console.log(err)
  console.log('connexion à la BD et on écoute sur le port 8081')
  })
